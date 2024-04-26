608b410923e966908508cb2923ad0118e3a1d9a5
Brance Sharp <107332360+brancesharp@users.noreply.github.com>
2024-04-25 21:35:01 -0700

    added css styling to header

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"net/http"
 
-	"blog-go/controllers"
-	"blog-go/templates"
-	"blog-go/views"
+	"lenslocked/controllers"
+	"lenslocked/templates"
+	"lenslocked/views"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -16,8 +16,14 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.html", "tailwind.html"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.html", "tailwind.html"))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.html", "tailwind.html"))))
+	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "signup.html", "tailwind.html"))))
 
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Statring the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
