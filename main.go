1922cf76e7cdb780cefc02c8c587ce6247d9bb7b
Brance Sharp <107332360+brancesharp@users.noreply.github.com>
2024-04-27 22:22:43 -0700

    added in new routes

--- main.go
+++ main.go
@@ -18,12 +18,8 @@ func main() {
 
 	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.html", "tailwind.html"))))
 	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.html", "tailwind.html"))))
-	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.html", "tailwind.html"))))
-	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "signup.html", "tailwind.html"))))
+	r.Get("/blog-posts", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "blog-posts.html", "tailwind.html"))))
 
-	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "Page not found", http.StatusNotFound)
-	})
 	fmt.Println("Statring the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
